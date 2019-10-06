5daaef79feedbd88ce11656c03d793c1f9807773
Sahil Mor <iconsahil7@gmail.com>
2019-10-06 22:55:30 +0530

    adding mongolab

--- models/showCampground.js
+++ models/showCampground.js
@@ -2,9 +2,9 @@ var Campground = require("./campgrounds")
 
 function ShowCampground(req,res){
    //req.user has the information of logged in user
-    Campground.find({},function(error,campgrounds){
+    Campground.find(function(error,campgrounds){
         if(error){
-            console.log("error")
+            console.log(error)
         }
         else{
                 res.render("index",{campgrounds : campgrounds})
